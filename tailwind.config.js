6e07aca650ad592e5a3cb825bcebcb44ce28bc1b
Emmanuel K. Buckley <ebuckley2389@gmail.com>
2024-02-12 23:23:48 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ['class'],
+  content: [
+    './index.html',
+    './src/components/**/*.{ts,tsx}',
+    './src/layouts/**/*.{ts,tsx}',
+    './src/pages/**/*.{ts,tsx}',
+    './src/utils/**/*.{ts,tsx}',
+  ],
+  plugins: [],
+};
